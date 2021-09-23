f248a8b6db3e29b5a010122d3d39ccdb21b6053e
1771009414huy <huy01682312920@gmail.com>
2021-09-23 08:59:55 +0700

    footer_all

---
+++ src/components/lab1/footer1.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Footer1() {
+    return (
+        <div className="footer">
+            <div className="title_footer">
+                <p>
+                    Option conasjdud Option conasjdud Option conasjdud Option
+                    conasjdud Option conasjdud{" "}
+                </p>
+            </div>
+            <div className="links_footer padding-15">
+                <div className="link_follow flex-1 padding_lf_ri-15 ">
+                    <h4>FOLLOW ON TWITTER</h4>
+                    <p>
+                        {" "}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Nisi, fugit pariatur. Ratione, aspernatur
+                        molestias aut nam tenetur cumque. Quibusdam,
+                        exercitationem quo tempore repellendus inventore a hic
+                        fugiat voluptatem nisi? Quod?
+                    </p>
+                    <button> Join</button>
+                </div>
+
+                <div className="link_lienket flex-3 padding_lf_ri-15 ">
+                    <div className="tilte_lienket">
+                        <h4>LIÊN KẾT</h4>
+                    </div>
+
+                    <ul>
+                        <li className="flex-1">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Aperiam aspernatur consequatur
+                                dolores aut earum autem,
+                            </p>
+                        </li>
+                        <li className="flex-1 ">
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur
+                                adipisicing elit
+                            </p>
+                        </li>
+                        <li className="flex-1">
+                            <p>
+                                Lorem ipsum, dolor sit amet consectetur
+                                adipisicing elit.{" "}
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="link_contact flex-1 padding_lf_ri-15">
+                    <p>CONTACT</p>
+                    <div className="TEL">PHONE: 03920329</div>
+                    <div className="TEXT">DSADADSNASJADHNA</div>
+                </div>
+            </div>
+            <div className="footer-last">
+                <b> @ Nature tour 2013.</b>
+                <p>Copy right and design by </p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer1;
